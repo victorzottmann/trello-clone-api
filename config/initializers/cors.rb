26b267c5c67b9f75f20155ed1e9a2851ce05ef1e
victorzottmann <zottmann.victor@gmail.com>
2021-06-26 20:27:41 +1000

    add frontend to CORS whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://affectionate-einstein-90cb4e.netlify.app'
 
     resource '*',
       headers: :any,
