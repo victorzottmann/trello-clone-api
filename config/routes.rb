7eddbdbc6837758771140be565096bfab7e405eb
victorzottmann <zottmann.victor@gmail.com>
2021-07-06 21:34:29 +1000

    create user model and sign up/in endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :cards
+  scope '/auth' do
+    post '/sign_up', to: 'users#create'
+    post '/sign_in', to: 'users#sign_in'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_234743) do
+ActiveRecord::Schema.define(version: 2021_07_06_104505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,15 @@ ActiveRecord::Schema.define(version: 2021_06_23_234743) do
   create_table "cards", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.boolean "completed"
+    t.boolean "completed", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
